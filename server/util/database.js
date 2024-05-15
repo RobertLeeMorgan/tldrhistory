3243c1dab70fa76712077b0444be324257cfe7d2
Rob Morgan <robleemorgan@gmail.com>
2024-05-15 17:10:29 +0200

    database host updated

--- server/util/database.js
+++ server/util/database.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require('sequelize')
 
 const sequelize = new Sequelize(
-    process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {dialect: 'mysql', host: 'localhost'}
+    process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {dialect: 'mysql', host: 'mysql-tldrhistory'}
 )
 
 module.exports = sequelize
