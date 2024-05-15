73217c004417eef17ad78c55a388c8726460be90
Rob Morgan <robleemorgan@gmail.com>
2024-05-15 17:23:08 +0200

    render blueprint

--- server/util/database.js
+++ server/util/database.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require('sequelize')
 
 const sequelize = new Sequelize(
-    process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {dialect: 'mysql', host: 'mysql-tldrhistory'}
+    process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {dialect: 'mysql', host: 'localhost'}
 )
 
 module.exports = sequelize
