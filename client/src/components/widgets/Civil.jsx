7a90f4858fc718d02ed9a7a60eec97808f15b635
Rob Morgan <robleemorgan@gmail.com>
2024-05-17 19:46:13 +0200

    removed undefined variable

--- client/src/components/widgets/Civil.jsx
+++ client/src/components/widgets/Civil.jsx
@@ -20,7 +20,7 @@ export default function Civil({ civil, year }) {
       className='overflow-hidden scroll-smooth'
       ref={tableRef}
     >
-      <Table display={display}civil={civil} />
+      <Table civil={civil} />
     </div>
   );
 }
