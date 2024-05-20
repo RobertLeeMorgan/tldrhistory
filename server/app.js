0a48612e27e4af3cef7ec9a1dbeb1fe98656f653
Rob Morgan <robleemorgan@gmail.com>
2024-05-20 22:57:58 +0200

    specified origins

--- server/app.js
+++ server/app.js
@@ -23,8 +23,13 @@ app.use(express.static("dist"));
 app.use(helmet());
 app.use(compression());
 
+const allowedOrigins = ['https://www.tldrhistory.xyz', 'https://www.tldrhistory.onrender.com'];
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
