415d126dcffdceae27c6ce72292bfa0b47a07f21
Rob Morgan <robleemorgan@gmail.com>
2025-10-13 00:02:34 +0800

    updated database initialisation for mysql2

--- server/util/database.js
+++ server/util/database.js
@@ -1,10 +1,12 @@
+
 const { Sequelize } = require("sequelize");
+const mysql2 = require('mysql2');
 
 const sequelize = new Sequelize(
   process.env.MYSQL_DATABASE,
   process.env.MYSQL_USER,
   process.env.MYSQL_PASSWORD,
-  { dialect: "mysql", host: process.env.MYSQL_HOST, logging: false, }
+  { dialect: "mysql", dialectModule: mysql2, host: process.env.MYSQL_HOST, logging: false, }
 );
 
 module.exports = sequelize;
